788c8797383947e0b35fe186d7a0958f5876439b
Benjamin Filiatrault <37634960+benjaminfiliatrault@users.noreply.github.com>
2023-08-26 07:53:50 -0400

    Term frequency calculation

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod read_lines;
 use std::{
     fs::{self, DirEntry},
     io,
-    path::Path,
+    path::{Path, PathBuf}, collections::HashMap, sync::Mutex,
 };
 
 use read_lines::read_lines;
@@ -30,6 +30,14 @@ impl<'a> Lexer<'a> {
             return token
     }
 
+    fn slice_while<P>(&mut self, mut predicate: P) -> &'a [char] where P: FnMut(&char) -> bool {
+        let mut idx = 0;
+        while idx < self.content.len() && predicate(&self.content[idx]) {
+            idx += 1;
+        }
+        self.slice(idx)
+    }
+
     fn next_token(&mut self) -> Option<&'a [char]> {
         self.trim_left();
 
@@ -38,20 +46,11 @@ impl<'a> Lexer<'a> {
         }
 
         if self.content[0].is_numeric() {
-            let mut idx = 0;
-            while idx < self.content.len() && self.content[idx].is_numeric() {
-                idx += 1;
-            }
-
-            return Some(self.slice(idx));
+            return Some(self.slice_while(|x| x.is_numeric()))
         }
 
         if self.content[0].is_alphabetic() {
-            let mut idx = 0;
-            while idx < self.content.len() && self.content[idx].is_alphanumeric() {
-                idx += 1;
-            }
-            return Some(self.slice(idx))
+            return Some(self.slice_while(|x| x.is_alphanumeric()))
         }
 
         return Some(self.slice(1))
@@ -65,10 +64,10 @@ impl<'a> Iterator for Lexer<'a> {
     }
 }
 
-fn get_file_content(path: &DirEntry) -> io::Result<String> {
+fn get_file_content(path: &Path) -> io::Result<String> {
     let mut content = String::new();
 
-    if let Ok(lines) = read_lines(path.path()) {
+    if let Ok(lines) = read_lines(path) {
         for line in lines {
             if let Ok(ip) = line {
                 content.push_str(&ip);
@@ -94,21 +93,43 @@ fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<()> {
     Ok(())
 }
 
+type TermFreq = HashMap<String, usize>;
+type DocIdx = HashMap<PathBuf, TermFreq>;
 
 fn main() {
     let dir_path = Path::new("react.dev/src/content");
+    let doc_idx = Mutex::new(DocIdx::new());
 
     let _ = visit_dirs(dir_path, &|path| {
-        if let Ok(content) = get_file_content(path) {
+        let mut term_frequency = TermFreq::new();
+        let file_path = path.path();    
+
+
+        if let Ok(content) = get_file_content(&file_path) {
             let content = content.chars().collect::<Vec<_>>();
 
             for chars in Lexer::new(&content) {
-                println!("{token:?}", token = chars.iter().map(|c| c.to_ascii_uppercase()).collect::<String>());
+
+               let term = chars.iter().map(|c| c.to_ascii_uppercase()).collect::<String>();
+
+               if let Some(frequency) = term_frequency.get_mut(&term) {
+                   *frequency += 1;
+               } else {
+                 term_frequency.insert(term, 1);
+               }
             }
+    
+            let mut stats = term_frequency.iter().collect::<Vec<_>>();
+
+            stats.sort_by_key(|(_, f)| *f);
+            stats.reverse();
+
+            doc_idx.lock().unwrap().insert(file_path, term_frequency);
         }
     });
 
-    // let content = get_file_content(file_path).expect("TODO");
 
-    // print!("{content}");
+    for (path, tf) in doc_idx.lock().unwrap().clone().into_iter() {
+       println!("{path:?} has {count} unique token", count = tf.len()); 
+    }
 }
