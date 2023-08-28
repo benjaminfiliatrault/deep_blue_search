69e560f4c38c498cbde8134f4a0b3c63a9021bd8
Benjamin Filiatrault <37634960+benjaminfiliatrault@users.noreply.github.com>
2023-08-28 07:30:02 -0400

    Batman

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 mod read_lines;
 
 use std::{
-    fs::{self, DirEntry},
-    io,
-    path::{Path, PathBuf}, collections::HashMap, sync::Mutex,
+    fs::{self, DirEntry, File},
+    path::{Path, PathBuf}, collections::HashMap, sync::Mutex, io::{self, stdin},
 };
 
 use read_lines::read_lines;
@@ -25,9 +24,9 @@ impl<'a> Lexer<'a> {
     }
 
     fn slice(&mut self, idx: usize) -> &'a [char] {    
-            let token = &self.content[0..idx];
-            self.content = &self.content[idx..];
-            return token
+        let token = &self.content[0..idx];
+        self.content = &self.content[idx..];
+        return token
     }
 
     fn slice_while<P>(&mut self, mut predicate: P) -> &'a [char] where P: FnMut(&char) -> bool {
@@ -96,40 +95,70 @@ fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<()> {
 type TermFreq = HashMap<String, usize>;
 type DocIdx = HashMap<PathBuf, TermFreq>;
 
-fn main() {
+fn main() -> io::Result<()> {
     let dir_path = Path::new("react.dev/src/content");
-    let doc_idx = Mutex::new(DocIdx::new());
+    let document_index_path = "index.json";
+    let document_index: DocIdx;
 
-    let _ = visit_dirs(dir_path, &|path| {
-        let mut term_frequency = TermFreq::new();
-        let file_path = path.path();    
+    if Path::new(document_index_path).exists() {
+   
+        println!("Reading from Index");
+
+        let index_file  = File::open(document_index_path)?;
+
+        document_index = serde_json::from_reader(index_file).unwrap();
+
+    } else {
+        let doc_idx = Mutex::new(DocIdx::new());
+        
+        let _ = visit_dirs(dir_path, &|path| {
+            let mut term_frequency = TermFreq::new();
+            let file_path = path.path();    
 
 
-        if let Ok(content) = get_file_content(&file_path) {
-            let content = content.chars().collect::<Vec<_>>();
+            if let Ok(content) = get_file_content(&file_path) {
+                let content = content.chars().collect::<Vec<_>>();
 
-            for chars in Lexer::new(&content) {
+                for chars in Lexer::new(&content) {
 
-               let term = chars.iter().map(|c| c.to_ascii_uppercase()).collect::<String>();
+                    let term = chars.iter().map(|c| c.to_ascii_uppercase()).collect::<String>();
 
-               if let Some(frequency) = term_frequency.get_mut(&term) {
-                   *frequency += 1;
-               } else {
-                 term_frequency.insert(term, 1);
-               }
+                    if let Some(frequency) = term_frequency.get_mut(&term) {
+                        *frequency += 1;
+                    } else {
+                        term_frequency.insert(term, 1);
+                    }
+                }
+
+                let mut stats = term_frequency.iter().collect::<Vec<_>>();
+
+                stats.sort_by_key(|(_, f)| *f);
+                stats.reverse();
+
+                doc_idx.lock().unwrap().insert(file_path, term_frequency);
             }
+        });
+
+        document_index = doc_idx.lock().unwrap().clone();
+
+        let index_file = File::create(document_index_path)?;
+
+        serde_json::to_writer(index_file, &document_index).expect("Cannot write document_index to disk"); 
+
+    };
+
+
+    println!("Index file contains {file_count}", file_count = document_index.len());
+
     
-            let mut stats = term_frequency.iter().collect::<Vec<_>>();
+    let mut query_input = String::new();
+    println!("Search: ");
 
-            stats.sort_by_key(|(_, f)| *f);
-            stats.reverse();
+    let query = stdin().read_line(&mut query_input).unwrap();
 
-            doc_idx.lock().unwrap().insert(file_path, term_frequency);
-        }
-    });
+    println!("{query_input}");
 
+    
 
-    for (path, tf) in doc_idx.lock().unwrap().clone().into_iter() {
-       println!("{path:?} has {count} unique token", count = tf.len()); 
-    }
+    Ok(())
 }
